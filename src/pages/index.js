3d52c32c40e66bdf325a74c7f6cdde2f24f05b8a
王成 <274281610@qq.com>
2019-09-26 17:56:43 +0800

    跳转

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,30 @@
 import styles from './index.css';
+import { Table, Button } from 'antd';
+import { Link } from 'react-router-dom';
+import router from 'umi/router';
+import { Component } from 'react';
 
-export default function() {
-  return (
-    <div className={styles.normal}>
-      <div className={styles.welcome} />
-      <ul className={styles.list}>
-        <li>To get started, edit <code>src/pages/index.js</code> and save to reload.</li>
-        <li>
-          <a href="https://umijs.org/guide/getting-started.html">
-            Getting Started 
-          </a>
-        </li>
-      </ul>
-    </div>
-  );
+class Index extends Component {
+
+  toTable(){
+    router.push('/tabledemo')
+  }
+
+  render(){
+    return (
+      <div className={styles.normal}>
+        <div className={styles.welcome} />
+        <ul className={styles.list}>
+          <li>To get started, edit <code>src/pages/index.js</code> and save to reload.</li>
+          <li>
+            <a href="https://umijs.org/guide/getting-started.html">
+              Getting Started 
+            </a>
+          </li>
+        </ul>
+        <Button onClick={this.toTable}>Table</Button>
+      </div>
+    )
+  }
 }
+export default Index;
