41d00ee661362c253f2544b2571d40f67c11e5a4
王成 <274281610@qq.com>
2019-10-10 15:13:59 +0800

    分页

--- src/pages/tabledemo/index.js
+++ src/pages/tabledemo/index.js
@@ -1,5 +1,5 @@
 import styles from './index.less';
-import { Table, Button ,Divider, Card,Tabs, Row, Col} from 'antd';
+import { Table, Button ,Divider, Card,Tabs, Row, Col,Statistic,Pagination} from 'antd';
 import React, { Component } from 'react'
 import { black } from 'ansi-colors';
 const { TabPane } = Tabs;
@@ -27,22 +27,48 @@ class TableDemo extends React.Component {
           time:'2019.10.10',
         },
       ],
+
+      dataSource1 : [
+        {
+          key: '1',
+          project_number: 1001,
+          project_name:1003,
+          gg_amount: 32,
+          gg_amount_car: 1000,
+          zj_amount:1001,
+          zj_amount_car:1002,
+        },
+        {
+          key: '2',
+          project_number: 1001,
+          project_name:1003,
+          gg_amount: 32,
+          gg_amount_car: 1000,
+          zj_amount:1001,
+          zj_amount_car:1002,
+        },
+      ],
+      
       
        columns : [
         {
           title: '所在区块高度',
           dataIndex: 'height',
           key: 'height',
+          width: '10%',
         },
         {
           title: '流水编号',
           dataIndex: 'number',
           key: 'number',
+          width: '10%',
+         
         },
         {
           title: '金额(元)',
           dataIndex: 'amount',
           key: 'amount',
+          width: '10%',
         },
         {
           title: '划账类型',
@@ -58,9 +84,63 @@ class TableDemo extends React.Component {
           title: '操作',
           dataIndex: 'action',
           key: 'action',
+          width: '10%',
           render: (text, item) => (
             <span>
               <a onClick={this.addFun.bind(this,item.number)}>详情</a>
+            </span>
+          ),
+        },
+      ],
+
+      columns1 : [
+        {
+          title: '项目编号',
+          dataIndex: 'project_number',
+          key: 'project_number',
+          width: '10%',
+        },
+        {
+          title: '项目名称',
+          dataIndex: 'project_name',
+          key: 'project_name',
+          width: '10%',
+         
+        },
+        {
+          title: '共管账户余额',
+          dataIndex: 'gg_amount',
+          key: 'gg_amount',
+          width: '10%',
+        },
+        {
+          title: '共管账户可用于经营性购车划款余额',
+          dataIndex: 'gg_amount_car',
+          key: 'gg_amount_car',
+          width: '20%',
+        },
+        {
+          title: '租金归集账户余额',
+          dataIndex: 'zj_amount',
+          key: 'zj_amount',
+          width: '10%',
+        },
+        {
+          title: '租金归集账户可用于经营性购车划款余额',
+          dataIndex: 'zj_amount_car',
+          key: 'zj_amount_car',
+          width: '20%',
+        },
+        {
+          title: '操作',
+          dataIndex: 'action',
+          key: 'action',
+          width: '10%',
+          render: (text, item) => (
+            <span>
+              <a onClick={this.addFun.bind(this,item.number)}>查看</a>
+              <Divider type="vertical" />
+              <a onClick={this.addFun.bind(this,item.number)}>划账</a>
             </span>
           ),
         },
@@ -86,14 +166,15 @@ class TableDemo extends React.Component {
     
             <Row  type="flex" justify="space-around" align="middle" style={{height:80,padding:10}} className={styles.row1}>
                 <Col span={4} >
-                    <div>
+                    {/* <div>
                       <div>
                         共管账户余额
                       </div>
-                      <div style={{color:black,fontSize:20,fontWeight:"bold"}}>
+                      <div style={{color:black,fontSize:20,fontWeight:"bold",fontFamily:"微软雅黑"}}>
                         100,000元
                       </div>
-                    </div>
+                    </div> */}
+                    <Statistic title="共管账户余额" value="100,000元" />
                 </Col>
                 <Divider type="vertical" style={{height:40}}></Divider>
               
@@ -102,7 +183,7 @@ class TableDemo extends React.Component {
                       <div>
                         共管账户可用于生产性经营余额
                       </div>
-                      <div style={{color:black,fontSize:20,fontWeight:"bold"}}>
+                      <div style={{color:black,fontSize:20,fontWeight:"bold",fontFamily:"微软雅黑"}}>
                         100,000元
                       </div>
                     </div>
@@ -114,7 +195,7 @@ class TableDemo extends React.Component {
                       <div>
                         租金归集账户余额
                       </div>
-                      <div style={{color:black,fontSize:20,fontWeight:"bold"}}>
+                      <div style={{color:black,fontSize:20,fontWeight:"bold",fontFamily:"微软雅黑"}}>
                         100,000元
                       </div>
                     </div>
@@ -125,7 +206,7 @@ class TableDemo extends React.Component {
                       <div>
                         租金归集账户可用于生产性经营余额
                       </div>
-                      <div style={{color:black,fontSize:20,fontWeight:"bold"}}>
+                      <div style={{color:black,fontSize:20,fontWeight:"bold",fontFamily:"微软雅黑"}}>
                         100,000元
                       </div>
                     </div>   
@@ -136,10 +217,10 @@ class TableDemo extends React.Component {
           </div>
           <Tabs type="flex" justify="start" className={styles.tabs}>
             <TabPane tab="共管账户转账记录" key="1" type="flex" justify="start">
-              <Table dataSource={this.state.dataSource} columns={this.state.columns}  />
+              <Table dataSource={this.state.dataSource} columns={this.state.columns}  pagination={false}/>
             </TabPane>
             <TabPane tab="租金归集账户转账记录" key="2" type="flex" justify="start">
-              <Table dataSource={this.state.dataSource} columns={this.state.columns}  />
+              <Table dataSource={this.state.dataSource1} columns={this.state.columns1} pagination={ false } />
             </TabPane>
           </Tabs>
       </div>
